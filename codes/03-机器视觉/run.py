fb4cbc27dea944888b1575070f845ef4581d49ce
hughxusu <hughxusu@qq.com>
2025-06-26 10:40:21 +0800

    增加课程代码

---
+++ codes/03-机器视觉/run.py
@@ -0,0 +1,55 @@
+from torchvision import datasets, transforms
+from utils import train_val_split
+from utils import PackDataset, ConvRelu, LinerRelu, control_callbacks
+from torch import nn
+import torch
+from skorch import NeuralNetClassifier
+
+full = datasets.FashionMNIST(root='./data', train=True, download=True)
+test = datasets.FashionMNIST(root='./data', train=False, download=True)
+train, valid = train_val_split(full)
+
+trans = transforms.Compose([transforms.Resize(size=67), transforms.ToTensor()])
+train_data = PackDataset(train, transform=trans)
+valid_data = PackDataset(valid, transform=trans)
+test_data = PackDataset(test, transform=trans)
+
+class AlexNetSmall(nn.Module):
+    def __init__(self, dropout=0.5):
+        super(AlexNetSmall, self).__init__()
+        self.c1 = ConvRelu(1, 96, kernel_size=11, stride=4)
+        self.p1 = nn.MaxPool2d(3, 2)
+        self.c2 = ConvRelu(96, 256, kernel_size=5, padding=2)
+        self.p2 = nn.MaxPool2d(3, 2)
+        self.c3 = ConvRelu(256, 384, kernel_size=3, padding=1)
+        self.c4 = ConvRelu(384, 384, kernel_size=3, padding=1)
+        self.c5 = ConvRelu(384, 256, kernel_size=3, padding=1)
+        self.p3 = nn.MaxPool2d(3, 2)
+        self.flatten = nn.Flatten()
+        self.l1 = LinerRelu(256, 128, dropout=dropout)
+        self.l2 = LinerRelu(128, 128, dropout=dropout)
+        self.l3 = LinerRelu(128, 10)
+
+    def forward(self, x):
+        x = self.p1(self.c1(x))
+        x = self.p2(self.c2(x))
+        x = self.p3(self.c5(self.c4(self.c3(x))))
+        x = self.flatten(x)
+        x = self.l3(self.l2(self.l1(x)))
+        return x
+
+epochs = 100
+ctrl = control_callbacks(epochs, check_dir='./data/alex-checkpoints')
+net = NeuralNetClassifier(
+    AlexNetSmall,
+    criterion=nn.CrossEntropyLoss,
+    optimizer=torch.optim.Adam,
+    lr=0.001,
+    batch_size=2048,
+    max_epochs=epochs,
+    train_split=lambda ds: (train_data, valid_data),
+    classes=list(range(10)),
+    device='cuda' if torch.cuda.is_available() else 'cpu',
+    callbacks=ctrl
+)
+net.fit(X=train_data, y=None)
